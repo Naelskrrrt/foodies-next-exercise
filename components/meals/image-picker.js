15cad0b6f3f5a0bad2a813d7cc41b6a805872003
NaelSkrrrt <ralalanael@gmail.com>
2024-03-17 10:58:58 +0300

    17/03/2024

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -7,15 +7,25 @@ import Image from "next/image";
 export default function ImagePicker({ label, name }) {
 	let [pickedImage, setPickedImage] = useState();
 	let imageInputRef = useRef();
+	let testRef = useRef();
+
+	
+
 	let handlePickClick = () => {
 		console.log(imageInputRef);
 		imageInputRef.current.click();
 	};
 
+	// Image preview event:
+	// 1. When user selects an image, it will be read and displayed in the preview section.
+	// 2. If user cancels the file picker, it will display "No image picked yet".
+
 	let handleImageChange = (event) => {
+		console.log(event)
 		const file = event.target.files[0];
 
 		if (!file) {
+			setPickedImage(null)
 			return;
 		}
 
@@ -48,6 +58,7 @@ export default function ImagePicker({ label, name }) {
 					name={name}
 					ref={imageInputRef}
 					onChange={handleImageChange}
+					required
 				/>
 				<button
 					className={classes.button}
@@ -57,6 +68,7 @@ export default function ImagePicker({ label, name }) {
 					Pick an Image
 				</button>
 			</div>
+			
 		</div>
 	);
 }
